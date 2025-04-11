2d2372c6906ea1bff0d4a1e1d1c101d6b4a4e72b
Jonathan Mumm <jonathanrmumm@gmail.com>
2025-04-10 19:03:00 -0700

    add the app bridge

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,58 @@
+import {
+  createNativeBridge,
+  createNativeBridgeContext,
+  createStore,
+} from "@open-game-system/app-bridge-react-native";
+import React from "react";
 import { Text, View } from "react-native";
+import { WebView } from "react-native-webview";
+
+type AppStores = {
+  castKit: {
+    state: {
+      isCasting: boolean;
+    };
+    events: {
+      type: "CAST";
+    };
+  };
+};
+
+const bridge = createNativeBridge<AppStores>();
+
+const store = createStore({
+  initialState: {
+    isCasting: true,
+  },
+});
+
+bridge.setStore("castKit", store);
+
+const BridgeContext = createNativeBridgeContext<AppStores>();
+
+const CastContext = BridgeContext.createNativeStoreContext("castKit");
 
 export default function Index() {
+  const CastStatus = () => {
+    const isCasting = CastContext.useSelector((state) => state.isCasting);
+
+    return <Text>{isCasting ? "Casting" : "Not Casting"}</Text>;
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <BridgeContext.BridgeProvider bridge={bridge}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <CastContext.StoreProvider>
+          <CastStatus />
+          <WebView source={{ uri: "https://triviajam.tv" }} />
+        </CastContext.StoreProvider>
+      </View>
+    </BridgeContext.BridgeProvider>
   );
 }
